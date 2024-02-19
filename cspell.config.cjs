b220f09b60168d9076f421552adfcde91e088954
electrovir <electrovir@users.noreply.github.com>
2024-02-19 14:11:48 +0000

    [patch] update deps

--- cspell.config.cjs
+++ cspell.config.cjs
@@ -2,12 +2,14 @@ const {baseConfig} = require('virmator/base-configs/base-cspell.js');
 
 module.exports = {
     ...baseConfig,
-    ignorePaths: [...baseConfig.ignorePaths],
+    ignorePaths: [
+        ...baseConfig.ignorePaths,
+    ],
     words: [
         ...baseConfig.words,
-        'farmerpaul',
-        'Robinfr',
         'espree',
+        'farmerpaul',
         'meriyah',
+        'robinfr',
     ],
 };
